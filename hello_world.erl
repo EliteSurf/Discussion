1c5da1433b7e479674867a1d4d2c1b78cfc59106
坏坏丶乔乔 <33797234+zhengyi96@users.noreply.github.com>
2018-02-27 10:47:29 +0800

    Add files via upload

---
+++ hello_world.erl
@@ -0,0 +1,15 @@
+%% @author User
+%% @doc @todo Add description to 'hello_world'.
+
+-module(hello_world).
+
+-import(string, [len/1, concat/2, chr/2, substr/3, str/2, 
+				 to_lower/1, to_upper/1]).
+
+-export([hou_tak/0]).
+
+hou_tak() ->
+	io:fwrite("Hello World to Hou Tak !!\n").
+
+
+
